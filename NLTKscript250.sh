9fedfee4037f796a9ccb63de39dc2a1eb36f4e7e
Rachel Truong <truongr@sandiego.edu>
2018-04-17 15:15:28 -0700

    Made several changes
    - Split words_per_line.txt into chunks of 250 words
    - Moved the 500 word chunks and 250 word chunks into separate folders
    underneath dir data
    - Created four scripts: NLTKscript250, NLTKscript500, princescript250,
    princescript500. NLTK and prince indicate different stop word lists and
    500 and 250 indicate which chunks are examined.

---
+++ NLTKscript250.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mallet import-dir --input data/Jekyll250 --output jekyll-input.mallet --keep-sequence --stoplist-file NLTKstopwords.txt
+mallet train-topics --optimize-interval 10 --num-topics 20 --num-iterations 1000 --input jekyll-input.mallet --output-topic-keys jekyll-20-topic-keys.txt --num-top-words 10
