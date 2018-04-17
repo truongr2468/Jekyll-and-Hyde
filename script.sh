9fe90f70cc7d971e6509112885ef3e1025994c1e
Paul Evans <decretist@gmail.com>
2018-04-16 22:13:33 -0700

    Edit script.sh
    Changed data directory and removed command-line argument for stopword
    processing.

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mallet import-dir --input data/jekyll --output jekyll-input.mallet --keep-sequence --stoplist-file stopwords.txt
+mallet import-dir --input data --output jekyll-input.mallet --keep-sequence
 mallet train-topics --optimize-interval 10 --num-topics 20 --num-iterations 1000 --input jekyll-input.mallet --output-topic-keys jekyll-20-topic-keys.txt --num-top-words 10
