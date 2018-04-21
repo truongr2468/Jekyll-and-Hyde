8e6599ef4ae97f765384cb9c290bdee82c52ac31
Rachel Truong <truongr@sandiego.edu>
2018-04-21 13:42:38 -0700

    Created folders for each type of plot and moved them accordingly.

--- jekyll.js
+++ jekyll.js
@@ -21,13 +21,33 @@ document.getElementById("defaultOpen").click();
 function nltk500 () {
     var x = document.createElement("IMG")
     if(document.getElementById('n500select').value == "Topic 0") {
-document.getElementById("x").src="R-Files/Final/R-Plots/Topic0.png";
+document.getElementById("x").src="R-Files/Final/R-Plots/NLTK500/Topic0.png";
     }
     else if(document.getElementById('n500select').value == "Topic 1") {
-      document.getElementById("x").src="R-Files/Final/R-Plots/Topic1.png";
+      document.getElementById("x").src="R-Files/Final/R-Plots/NLTK500/Topic1.png";
     }
     else if(document.getElementById('n500select').value == "Topic 2") {
-      document.getElementById("x").src="R-Files/Final/R-Plots/Topic2.png";
+      document.getElementById("x").src="R-Files/Final/R-Plots/NLTK500/Topic2.png";
+    }
+    else if(document.getElementById('n500select').value == "Topic 3") {
+      document.getElementById("x").src="R-Files/Final/R-Plots/NLTK500/Topic3.png";
+    }
+    else if(document.getElementById('n500select').value == "Topic 4") {
+      document.getElementById("x").src="R-Files/Final/R-Plots/NLTK500/Topic4.png";
     }
     document.body.appendChild(x);
+}
+
+function nltk250 () {
+    var y = document.createElement("IMG")
+    if(document.getElementById('n250select').value == "Topic 0") {
+document.getElementById("y").src="R-Files/Final/R-Plots/NLTK250/Topic0.png";
+    }
+    else if(document.getElementById('n250select').value == "Topic 1") {
+      document.getElementById("y").src="R-Files/Final/R-Plots/NLTK250/Topic1.png";
+    }
+    else if(document.getElementById('n250select').value == "Topic 2") {
+      document.getElementById("y").src="R-Files/Final/R-Plots/NLTK250/Topic2.png";
+    }
+    document.body.appendChild(y);
 }
